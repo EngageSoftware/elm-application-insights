9c36cecb310c2c5126dd2d705f631e44c16f271b
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2016-10-21 07:04:24 -0500

    fix invalid name

--- src/Native/AppInsights.js
+++ src/Native/AppInsights.js
@@ -1,12 +1,12 @@
 var _EngageSoftware$elm_application_insights$Native_AppInsights = function() {
 
     function trackEvent(data) {
-        if (!window.appInsight) {
+        if (!window.appInsights) {
             return data;
         } 
 
         var str = _elm_lang$core$Basics$toString(data);
-        window.appInsight.trackEvent(str);
+        window.appInsights.trackEvent(str);
         return data;
     }
 
